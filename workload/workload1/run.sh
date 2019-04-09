cde9930ee981f1252f324d879dd73bfaa365c238
Andrew <xzhan99@hotmail.com>
2019-04-10 04:12:56 +1000

    adapt to emr-5.21.0

--- workload/workload1/run.sh
+++ workload/workload1/run.sh
@@ -6,7 +6,7 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-hadoop jar /usr/lib/hadoop/hadoop-streaming-2.8.5-amzn-2.jar \
+hadoop jar /usr/lib/hadoop/hadoop-streaming-2.8.5-amzn-1.jar \
 -D mapreduce.job.maps=3 \
 -D mapreduce.job.reduces=1 \
 -D mapreduce.job.name='Task1' \
